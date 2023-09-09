db81ac10d51c5d456af5e13a0da21bd4043a384f
ainotil <ruotongtoniali@gmail.com>
2023-09-09 21:13:28 +1000

    done

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time
+
+st.set_page_config(layout="wide", page_title="CV for Stryker")
+
+st.write("#### Hi")
+time.sleep(1)
+st.write("#### This is Tonia Li, a 2nd Year Computational Data Science Student at UNSW")
+time.sleep(1)
+st.write("#### I noticed that there's no session for cover letter on the application page, but I feel it's necessary to express my excites with this opportunity as possibly working at Stryker :)")
+time.sleep(2)
+st.write("##### Days ago I attended UNSW WIT Hackathon 2023 which held a theme of healthcare and won 3rd place, by developing a software in a group of 3 which is able to convert incomprehensible medical lab reports to human readable format with graphs. It allows the users to view the fluctuation of their health, by calculating a health score. Even though it was a really rough software as we only had 24 hours to build it, including a pitching session, however it was still a precious and inspirational learning experience that I did lots of research on what medical devices and services people might need nowadays, and came up with this idea. I believe my background and passion align well with the mission of improving healthcare outcomes through innovation and technology. I love challenges and also a critical thinker who can observe more details and weakness of objects. I'm thrilled with this opportunity and all potentials of working at Stryker, as you are one of the leading companies in the healthcare industry, specializing in the development, manufacturing, and marketing of a wide range of medical devices and equipment!")
+st.write("Here's my GitHub of the project in Hackathon if you're interested: https://github.com/ainotil/WIT-Healthcare")
